c32cdf0121fcc8a7f9f4536f28be830e4af5d4af
Cesar <c.caycho46@gmail.com>
2021-07-13 19:27:47 -0500

    listo

--- shell.sh
+++ shell.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-while getopts ":d: g" opt; do
+while getopts ":d: f g j" opt; do
     case $opt in
     d)
-        path=$OPTARG/
+        path=$OPTARG
         ;;
     g)
-        go="1"
+        type="go"
+        ;;
+    j)
+        type="js"
         ;;
     \?)
         echo "Invalid option: -$OPTARG" >&2
@@ -16,7 +19,6 @@ while getopts ":d: g" opt; do
 done
 
 if [ -z "$path" ]; then
-    echo "no"
     path=$(pwd)
 fi
 
@@ -25,8 +27,9 @@ if [ ! -e "$path" ]; then
     exit 2
 fi
 
-if [ ! -z $go ]; then
-    cat >./test/main.go <<limite
+case $type in
+"go")
+    cat >"$path"/main.go <<limite
 package main
 
 import(
@@ -37,4 +40,54 @@ func main(){
     fmt.Println("Hello World")
 }
 limite
-fi
+
+    cat >"$path"/main_test.go <<limite
+package main
+
+import(
+    "fmt"
+    "testing"
+)
+
+func TestHelloWorld(t *testing.T){
+    fmt.Println("hola")
+}
+limite
+    go mod init 2>/dev/null
+    ;;
+
+"js")
+    cat >"$path"/index.html <<limite
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js" defer></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
+limite
+    mkdir "$path"/src "$path"/dist
+    mkdir "$path"/dist/css "$path"/dist/js "$path"/src/js "$path"/src/sass
+    cat >"$path"/src/js/index.js <<limite
+    console.log("hello world")
+limite
+    cat >"$path"/src/sass/style.scss <<limite
+    *{
+        margin: 0;
+    }
+limite
+    npm init -y -w "$path"
+    ;;
+*)
+    echo "You need to specify what type of proyect you will be doing"
+    ;;
+esac
